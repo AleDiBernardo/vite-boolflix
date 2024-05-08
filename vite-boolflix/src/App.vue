8bda8c4cae739c60464c335d282d9389d2088cd4
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-05-08 12:27:40 +0200

    feat: store and api test done

--- vite-boolflix/src/App.vue
+++ vite-boolflix/src/App.vue
@@ -1,4 +1,30 @@
 <script>
+import {store} from "./store";
+import axios from "axios";
+export default{
+    data(){
+        return{
+            store,
+        }
+    },
+    created(){
+        this.getMovie()
+    },
+    methods:{
+        getMovie(){
+            const params = {
+                query: "kung fu panda",
+                api_key: "bca2cd4e0092d2eac7fdb4f97170e2fb",
+                language: "it-IT"
+            }
+
+            axios.get("https://api.themoviedb.org/3/search/movie",{params}).then((resp)=>{
+                this.store.movieList = resp.data.results;
+                console.log(this.store.movieList);
+            })
+        }
+    }
+}
 </script>
 
 <template>
