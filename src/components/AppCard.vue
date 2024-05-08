121f738f81eca22deff46c6d4cfde98ba20b5131
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-05-08 14:29:56 +0200

    feat: some design

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -11,14 +11,19 @@ export default {
 
 <template>
   <div class="card">
-    <h4>{{ title }}</h4>
-    <h5>{{ originalTitle }}</h5>
-    <span>{{ lang }}</span>
-    <span>{{ vote_avg }}</span>
+    <h4>Title: {{ title }}</h4>
+    <h5>Original Title: {{ originalTitle }}</h5>
+    <h6>Original Language: {{ lang }}</h6>
+    <h6>Vote: {{ vote_avg }}</h6>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @use "../style/partials/mixins" as *;
 @use "../style/partials/variables" as *;
+
+.card{
+    @include flex(column,flex-start,flex-start);
+    height: 100%;    
+}
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,42 +1,41 @@
 <script>
 import AppCard from "./AppCard.vue";
-import {store} from "../store.js" 
-export default{
-    components:{
-        AppCard
-    },
-    data(){
-        return{
-            store
-        }
-    }
-}
+import { store } from "../store.js";
+export default {
+  components: {
+    AppCard,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+};
 </script>
 
 <template>
-<main>
+  <main>
     <div class="container">
-        <div class="row">
-            <div class="col" v-for="curMovie in store.movieList">
-                <AppCard
-                    :title="curMovie.title"
-                    :originalTitle="curMovie.original_title"
-                    :lang="curMovie.original_language"
-                    :vote_avg="curMovie.vote_average"
-                />
-            </div>
+      <div class="row row-cols-lg-4 row-cols-md-3 row-cols-2">
+        <div class="col p-2" v-for="curMovie in store.movieList">
+          <AppCard
+            class="p-2"
+            :title="curMovie.title"
+            :originalTitle="curMovie.original_title"
+            :lang="curMovie.original_language"
+            :vote_avg="curMovie.vote_average"
+          />
         </div>
+      </div>
     </div>
-</main>
+  </main>
 </template>
 
 <style lang="scss" scoped>
 @use "../style/partials/mixins" as *;
 @use "../style/partials/variables" as *;
 
-.container{
-    @include flex(row,space-between,center);
-
+.container {
+  @include flex(row, space-between, center);
 }
-
 </style>
