d28dcc562f404acfc3905f4e4ae5414d9c352153
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-05-09 17:09:47 +0200

    feat: some changes to navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,106 +1,119 @@
 <script>
-import AppSearchBar from "./AppSearchBar.vue"
-import {store} from "../store.js" 
-import axios from "axios"
+import AppSearchBar from "./AppSearchBar.vue";
+import { store } from "../store.js";
+import axios from "axios";
 
-export default{
-    data(){
-        return{
-            store,
-            links: ["Home","TV Series","Films","Original","Recently Added", "My Watchlist"],
-            activeIndex: 0
-        }
-    },
-    components:{
-        AppSearchBar
-    },
-    methods:{
-        getResult(){
-            if (this.store.userQuery !== '') {
-                
-                const params = {
-                    query: this.store.userQuery,
-                    api_key: "bca2cd4e0092d2eac7fdb4f97170e2fb",
-                    language: "it-IT"
-                }
-    
-                axios.get("https://api.themoviedb.org/3/search/movie",{params}).then((resp)=>{
-                    this.store.movieList = resp.data.results;
-                    console.log("Movie: " + this.store.movieList);
-                })
-                axios.get("https://api.themoviedb.org/3/search/tv",{params}).then((resp)=>{
-                    this.store.seriesList = resp.data.results;
-                    console.log("Series: " + this.store.seriesList);
-                })
-    
-                this.store.userQuery = ""
-                this.store.placeHolder = "Es: Harry Potter"
+export default {
+  data() {
+    return {
+      store,
+      links: [
+        "Home",
+        "TV Series",
+        "Movies",
+        "Original",
+        "Recently Added",
+        "My Watchlist",
+      ],
+      activeIndex: 0,
+    };
+  },
+  components: {
+    AppSearchBar,
+  },
+  methods: {
+    getResult() {
+      if (this.store.userQuery !== "") {
+        const params = {
+          query: this.store.userQuery,
+          api_key: "bca2cd4e0092d2eac7fdb4f97170e2fb",
+          language: "it-IT",
+        };
 
-            } else{
-                this.store.placeHolder = "Cannot be empty"
-            }
-            
-        },
-        activateLink(index) {
-        if (this.activeIndex !== null) {
-            this.activeIndex = null;
-        }
-        this.activeIndex = index;
-    }
-    }
-}
+        axios
+          .get("https://api.themoviedb.org/3/search/movie", { params })
+          .then((resp) => {
+            this.store.movieList = resp.data.results;
+            console.log("Movie: " + this.store.movieList);
+          });
+        axios
+          .get("https://api.themoviedb.org/3/search/tv", { params })
+          .then((resp) => {
+            this.store.seriesList = resp.data.results;
+            console.log("Series: " + this.store.seriesList);
+          });
+
+        this.store.userQuery = "";
+        this.store.placeHolder = "Es: Harry Potter";
+      } else {
+        this.store.placeHolder = "Cannot be empty";
+      }
+    },
+    activateLink(index) {
+      if (this.activeIndex !== null) {
+        this.activeIndex = null;
+      }
+      this.activeIndex = index;
+    },
+  },
+};
 </script>
 
 <template>
-<header>
-<div class="container">
-    <div class="navbar">
-
+  <header>
+    <div class="container">
+      <div class="navbar">
         <a href="#" class="logo">
-            <img src="../assets/logo.png" alt="">
+          <img src="../assets/logo.png" alt="" />
         </a>
-    <ul class="d-flex flex-row gap-3">
-        <li v-for="(link,index) in links" @click="activateLink(index)"><a href="#" :class="{active : index === activeIndex}">{{ link }}</a></li>
-    </ul>
+        <ul class="d-flex flex-row gap-3">
+          <li v-for="(link, index) in links" @click="activateLink(index)">
+            <a href="#" :class="{ active: index === activeIndex }">{{
+              link
+            }}</a>
+          </li>
+        </ul>
+      </div>
+      <AppSearchBar @filter="getResult" />
     </div>
-    <AppSearchBar @filter="getResult"/>
-    
-</div>
-</header>
+  </header>
 </template>
 
 <style lang="scss" scoped>
 @use "../style/partials/mixins" as *;
 @use "../style/partials/variables" as *;
 
+header {
+  @include flex(row, center, center);
 
+  width: 100%;
+  height: $header-heigth;
+  padding: 10px 0;
+  background-color: $primary-color;
 
-header{
-    @include flex(row,center,center);
+  .container {
+    @include flex(row, space-between, center);
 
-    width: 100%;
-    height: $header-heigth;
-    padding: 10px 0;
-    background-color: $primary-color;
-    
-
-    
-    .container{
-        @include flex(row,space-between,center);
-
-        .logo{
-            max-width: 150px;
-        }
-        ul{
-            align-self: center;
-            margin: 0;
-        }
-
-        .active {
-            color: red;
-        }
+    .logo {
+      max-width: 150px;
     }
+    ul {
+      align-self: center;
+      margin: 0;
+      li {
+        a {
+          color: lightgray;
+          &:hover{
+            color: red;
+          }
+        }
+      }
+    }
+
+    .active {
+      color: white;
+      font-weight: bold;
+    }
+  }
 }
-
-
 </style>
